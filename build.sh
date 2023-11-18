65b9461397b571f38e4abbfb870c5546f415c438
wr7 <53203261+wr7@users.noreply.github.com>
2023-11-18 12:44:22 -0600

    Initial commit

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 srcdir=.
-buildir=build/debug
+buildir=build
 
 if [ -d "$buildir" ]; then
     echo "removing directory: $buildir"
     rm -r $buildir
 fi
 
-meson $srcdir $buildir --cross-file config/cross_linux.txt --buildtype=debugoptimized && cd $buildir
+meson $srcdir $buildir --cross-file config/cross_linux.txt --buildtype=minsize && cd $buildir
--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CONTAINER="meson_env"
-
-docker build config/ -t $CONTAINER
-docker run -it -v $(pwd):/project -h build $CONTAINER bash 
