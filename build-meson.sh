ec8ca3513da789a668d3e1d20663512b1ecff2b5
wr7 <53203261+wr7@users.noreply.github.com>
2023-11-19 10:02:58 -0600

    Make build script run ninja

---
+++ build-meson.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+srcdir=.
+buildir=build
+
+if [ -d "$buildir" ]; then
+    echo "removing directory: $buildir"
+    rm -r $buildir
+fi
+
+meson $srcdir $buildir --cross-file config/cross_linux.txt --buildtype=minsize && cd $buildir
