dff70b13287b55b50c1e498fd7a228291a6ce7c5
wr7 <53203261+wr7@users.noreply.github.com>
2023-11-28 18:00:05 -0600

    Add border around screen to make the edges more visible

--- src/Src/snake_state.c
+++ src/Src/snake_state.c
@@ -54,22 +54,33 @@ void snake_state_tick(SnakeState *self, Button input) {
 void snake_state_initialize(SnakeState *self) {
   self->random_number_generator = prng_with_default_seed();
   self->snake_position = position_ring_buffer_new(self->snake_position_buffer, ARRAY_LENGTH(self->snake_position_buffer));
-  self->snake_direction = DIRECTION_RIGHT;
+  self->snake_direction = DIRECTION_DOWN;
 
-  position_ring_buffer_push(&self->snake_position, (Position){5, 5});
-  position_ring_buffer_push(&self->snake_position, (Position){5, 6});
-  position_ring_buffer_push(&self->snake_position, (Position){5, 7});
-  position_ring_buffer_push(&self->snake_position, (Position){5, 8});
-
+  // Clear screen //
   for(int i = 0; i < ARRAY_LENGTH(self->screen); i++) {
     self->screen[i] = 0;
   }
 
-  // Draw initial segments
+  // Create and draw initial snake segments //
+  position_ring_buffer_push(&self->snake_position, (Position){5, 5});
+  position_ring_buffer_push(&self->snake_position, (Position){5, 6});
+  position_ring_buffer_push(&self->snake_position, (Position){5, 7});
+
   for(int i = 0; i < self->snake_position.length; i++) {
     Position snake_segment = *position_ring_buffer_get(self->snake_position, i);
 
     screen_set_pixel(self->screen, snake_segment);
+  }
+
+  // Add border around screen to make the edges more visible //
+
+  for(u8 i = 0; i < 64; i++) {
+    screen_set_pixel(self->screen, (Position){i, 0});
+    screen_set_pixel(self->screen, (Position){i, 31});
+  }
+  for(u8 i = 1; i < 32; i++) {
+    screen_set_pixel(self->screen, (Position){0, i});
+    screen_set_pixel(self->screen, (Position){63, i});
   }
 
   respawn_fruit(self);
